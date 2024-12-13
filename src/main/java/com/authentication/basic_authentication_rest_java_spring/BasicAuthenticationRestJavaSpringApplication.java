a97da791c8001dde47ed409250a709cf6522a118
Miguel Monroy <miguelamh96@hotmail.com>
2024-12-13 09:57:08 -0500

    Inicializando el curso de authenticacion en Spring Bott

---
+++ src/main/java/com/authentication/basic_authentication_rest_java_spring/BasicAuthenticationRestJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.authentication.basic_authentication_rest_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuthenticationRestJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicAuthenticationRestJavaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/authentication/basic_authentication_rest_java_spring/BasicAuthenticationRestJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.basic_authentication_rest_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthenticationRestJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
